7331b8b7685cae3012f647e8460b094b2591a1ef
Grace Wang <grace.wang95@gmail.com>
2021-06-04 18:46:22 +0800

    add storage flag to ensure product availability

--- java_thread/consumer-producer/src/com/demo/Product.java
+++ java_thread/consumer-producer/src/com/demo/Product.java
@@ -4,6 +4,7 @@ public class Product {
 
     private String brand;
     private String name;
+    private Boolean storage = false;
 
     public String getName() {
         return name;
@@ -22,10 +23,31 @@ public class Product {
     }
 
     public synchronized void get() {
+        if (!storage) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.printf("The consumer has purchased a %s ---- %s \n", this.getBrand(), this.getName());
+        storage = false;
+        notify();
     }
 
-    public synchronized void set(String brand, String name) {
+    public synchronized void set(String brand, String name){
+        if (storage) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         this.setBrand(brand);
         try {
             Thread.sleep(500);
@@ -34,5 +56,7 @@ public class Product {
         }
         this.setName(name);
         System.out.printf("The producer has produced a %s ---- %s \n", this.getBrand(), this.getName());
+        storage = true;
+        notify();
     }
 }
